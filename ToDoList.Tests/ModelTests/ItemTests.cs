5997cddb1d94737acef8f5cff9bd9f4948c14bb2
Carlos Urquiza <webquiza@gmail.com>
2021-09-05 09:15:18 -0700

    update working test

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -1,18 +1,68 @@
+using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ToDoList.Models;
 
 namespace ToDoList.Tests
 {
   [TestClass]
-  public class ItemTests
+  public class ItemTests : IDisposable
   {
 
+		public void Dispose()
+    {
+      Item.ClearAll();
+    }
+		
 		[TestMethod]
 		public void ItemConstructor_CreatesInstanceOfItem_Item()
 		{
-			Item newItem = new Item();
+			Item newItem = new Item("test"); //We pass in "test" as an argument here.
 			Assert.AreEqual(typeof(Item), newItem.GetType());
 		}
 		
+		[TestMethod]
+    public void GetDescription_ReturnsDescription_String()
+    {
+			//Arrange
+      string description = "Walk the dog.";
+      Item newItem = new Item(description);
+
+			//Act
+      string result = newItem.Description;
+
+			//Assert
+      Assert.AreEqual(description, result);
+    }
+
+		[TestMethod]
+    public void SetDescription_SetDescription_String()
+    {
+      //Arrange
+      string description = "Walk the dog.";
+      Item newItem = new Item(description);
+
+      //Act
+      string updatedDescription = "Do the dishes";
+      newItem.Description = updatedDescription;
+      string result = newItem.Description;
+
+      //Assert
+      Assert.AreEqual(updatedDescription, result);
+    }
+
+		[TestMethod]
+		public void GetAll_ReturnsEmptyList_ItemList()
+		{
+			// Arrange
+			List<Item> newList = new List<Item> { };
+
+			// Act
+			List<Item> result = Item.GetAll();
+
+			// Assert
+			CollectionAssert.AreEqual(newList, result);
+		}
+
   }
 }
